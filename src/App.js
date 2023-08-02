cad796ba6c9eb992252173d4593bbe80f5e1751c
Cyril <fadaakin@outlook.com>
2023-08-02 10:51:00 +0100

    bus ticket app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Brand, Vision, Navbar } from "./components";
 import { Ticket, Header, Partners, Footer, AboutUs } from "./containers";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
-
 
 
 
