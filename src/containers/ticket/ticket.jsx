d554c2fe410e6240ca48449a17e5bc38dab1a42f
Cyril <fadaakin@outlook.com>
2023-08-02 17:44:40 +0100

    bus terminal webpage

--- src/containers/ticket/ticket.jsx
+++ src/containers/ticket/ticket.jsx
@@ -1,113 +1,102 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import ticketpix from '../../assests/mobileticket.jpg';
 import './ticket.css';
 
-class BusTicket extends Component {
-  state = {
-    passengerName: " ",
-    passengerEmail: " ",
-    seatNumber: " ",
-    fare: 0,
-    travelLocationFrom: " ",
-    travelLocationTo: " ",
-  };
+function Ticket() {
+  const [passengerName, setPassengerName] = useState("");
+  const [passengerEmail, setPassengerEmail] = useState("");
+  const [seatNumber, setSeatNumber] = useState("");
+  const [fare, setFare] = useState("");
+  const [travelLocationFrom, setTravelLocationFrom] = useState("");
+  const [travelLocationTo, setTravelLocationTo] = useState("");
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Passenger name:", this.state.passengerName);
-    console.log("Passenger email:", this.state.passengerEmail);
-    console.log("Seat number:", this.state.seatNumber);
-    console.log("Fare:", this.state.fare);
-    console.log("Travel location from:", this.state.travelLocationFrom);
-    console.log("Travel location to:", this.state.travelLocationTo);
+    console.log("Passenger name:", passengerName);
+    console.log("Passenger email:", passengerEmail);
+    console.log("Seat number:", seatNumber);
+    console.log("Fare:", fare);
+    console.log("Travel location from:", travelLocationFrom);
+    console.log("Travel location to:", travelLocationTo);
+    setPassengerName("");
+    setPassengerEmail("");
+    setSeatNumber("");
+    setFare("");
+    setTravelLocationFrom("");
+    setTravelLocationTo("");
   };
 
-  render() {
-    return (
-      <div className="trave__ticket" id="ticket">
-        
-
-        
+  return (
+    <div className="trave__ticket" id="ticket">
       <div className="mobile_picture">
         <img src={ticketpix} alt="ticket pix" />
       </div>
-        <div className="ticket__form">
-      <h1>Bus Ticket</h1>
-        <label>
-          <input
-            name="passengerName"
-            type="text"
-            placeholder="Passenger Name"
-            value={this.state.passengerString}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <label>
-          <input
-            name="passengerEmail"
-            type="email"
-            placeholder="Passenger Email"
-            value={this.state.passengerEmail}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <label>
-          <input
-            name="seatNumber"
-            type="text"
-            placeholder="Seat Number"
-            value={this.state.seatString}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <label>
-          <input
-            name="fare"
-            type="number"
-            placeholder="Fare"
-            value={this.state.fare}
-            onChange={this.handleChange}
-            disabled={true}
-          />
-        </label>
-
-        <label>
-          <input
-            name="traveLocationFrom"
-            type="text"
-            placeholder="Travel Location From"
-            value={this.state.travelLocationFromString}
-            onChange={this.handleChange}
-          />
-        </label>
-
-
-
-        <label>
-          <input
-            name="traveLocationTo"
-            type="text"
-            placeholder="Travel Location To"
-            value={this.state.travelLocationToString}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <button onClick={this.handleSubmit}>Book Ticket</button>
+      <div className="ticket__form">
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div className="ticket-form">
+              <h1>Bus Ticket</h1>
+              <label>
+                <input
+                  type="text"
+                  placeholder="Passenger Name"
+                  name="passengerName"
+                  value={passengerName}
+                  onChange={(e) => setPassengerName(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  type="text"
+                  placeholder="Passenger Email"
+                  name="passengerEmail"
+                  value={passengerEmail}
+                  onChange={(e) => setPassengerEmail(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  name="seatNumber"
+                  type="text"
+                  placeholder="Seat Number"
+                  value={seatNumber}
+                  onChange={(e) => setSeatNumber(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  name="fare"
+                  type="number"
+                  placeholder="Fare"
+                  value={fare}
+                  onChange={(e) => setFare(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  name="traveLocationFrom"
+                  type="text"
+                  placeholder="Travel Location From"
+                  value={travelLocationFrom}
+                  onChange={(e) => setTravelLocationFrom(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  name="traveLocationTo"
+                  type="text"
+                  placeholder="Travel Location To"
+                  value={travelLocationTo}
+                  onChange={(e) => setTravelLocationTo(e.target.value)}
+                />
+              </label>
+            </div>
+          </fieldset>
+          <button onClick={handleSubmit}>Book Ticket</button>
+        </form>
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default BusTicket;
+export default Ticket;
